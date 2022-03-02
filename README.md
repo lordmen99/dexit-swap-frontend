cf3aa5121a352058dae96d3c42acbba3b0de34c6
blockchain-vova <leyr1114@gmail.com>
2022-03-03 07:10:52 +1000

    commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# DeXit DeX template
+
+### DeXit Finance
+
+- Factory: 0xDd086f6AB31a1ea2921e7d73d37a8D98fb90E16C
+- Router: 0x8dbF0332F2b75Edd52bB2a8aEA2A93B9C3AC1b27
+- Pair hash: 0x0bbd2ea78a8677b013107b353a6d1c38ed7eb8d91ea3177957f05cc245774a58
---
+++ src/components/Identicon/StatusIcon.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NetworkConnector } from '../../connectors/NetworkConnector';
+import { AbstractConnector } from '@web3-react/abstract-connector';
+import CoinbaseWalletIcon from '../../assets/images/coinbaseWalletIcon.svg';
+import WalletConnectIcon from '../../assets/images/walletConnectIcon.svg';
+import { injected, walletconnect, walletlink } from '../../connectors';
+import Identicon from '../Identicon';
+
+export default function StatusIcon({ connector }: { connector: AbstractConnector | NetworkConnector }) {
+  switch (connector) {
+    case injected:
+      return <Identicon />;
+    case walletconnect:
+      return <img src={WalletConnectIcon} alt={'WalletConnect'} />;
+    case walletlink:
+      return <img src={CoinbaseWalletIcon} alt={'Coinbase Wallet'} />;
+    default:
+      return null;
+  }
+}
